8ac0fb8f3abf913268b2c20cc653be63ef4cc82f
Messivânio Galvão <messivanio.galvao@gmail.com>
2016-09-10 08:50:55 -0300

    Set up poltergeist and screenshots

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,8 +1,35 @@
 require 'capybara'
 require 'capybara/cucumber'
+require 'capybara/poltergeist'
+require 'capybara-screenshot/cucumber'
 
 Capybara.configure do |config|
-  config.default_driver = :selenium
-  config.app_host   = 'http://www.google.com'
+  config.app_host = ENV['APP_HOST'] || 'http://web_1:8080'
 end
+
+if ENV['IN_BROWSER']
+  Capybara.default_driver = :selenium
+  AfterStep do
+    sleep (ENV['PAUSE'] || 0).to_i
+  end
+else
+  Capybara.register_driver :poltergeist do |app|
+    Capybara::Poltergeist::Driver.new(
+      app,
+      window_size: [1280, 1024]#,
+      #debug:       true
+    )
+  end
+  Capybara.default_driver    = :poltergeist
+  Capybara.javascript_driver = :poltergeist
+end
+
+current_path = File.dirname(__FILE__)
+Capybara.save_path = "#{current_path}/../../screenshots"
+Capybara::Screenshot.prune_strategy = { keep: 3 }
+
+Before do |scenario|
+  visit('/')
+end
+
 World(Capybara)
